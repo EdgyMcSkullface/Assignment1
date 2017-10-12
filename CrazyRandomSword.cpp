4d0db1bb203258c63973c85bc1190ea10ba4c3cf
Bo Cameron <bogarrettcameron@gmail.com>
2017-10-11 21:11:42 -0400

    Resolved a linker error where modulus was called on a double, and a bug where the wrong weapon was called for Crazy Random Sword.

--- CrazyRandomSword.cpp
+++ CrazyRandomSword.cpp
@@ -3,7 +3,7 @@
 double CrazyRandomSword::hit(double armor) {
 	srand(time(NULL));
 	hitPoints = rand() % 90 + 10;
-	double armIgnored = rand() % ((armor/2) + 1);
+	double armIgnored = static_cast<int>(rand()) % static_cast<int>(((armor/2) + 1));
 	double damage = hitPoints - (armor - armIgnored);
     if (damage < 0) {
         return 0;
